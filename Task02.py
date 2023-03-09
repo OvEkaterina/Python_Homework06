93c2fb50654654c1b7d8d73a43401126b4fe6382
Ekaterina <sweetykiti@gmail.com>
2023-03-09 20:50:42 +0300

    Решение второй задачи

--- Task02.py
+++ Task02.py
@@ -1,15 +1,27 @@
 # Задача 32: Определить индексы элементов массива (списка), 
 # значения которых принадлежат заданному диапазону 
 # (т.е. не меньше заданного минимума и не больше заданного максимума)
-list= []
-print("Введите количество эллментов массива :")
-n = int(input())
-from random import randint
-for i in range(n):
-   list.append(randint(1, n)) 
-print(list)
-print("Введите значение х :")
-x = int(input())
-list_new =[abs(x-i) for i in list]
-print(list[list_new.index(min(list_new))])
 
+def array_random(n, min, max):
+    list = []
+    from random import randint
+    for i in range(n):
+        list.append(randint(min, max)) 
+    return(list)
+
+print("Введите количество элементов массива :")
+n = int(input())
+print("Введите минимальное значение элемента массива :")
+min_array = int(input())
+print("Введите максимальное значение элемента массива :")
+max_array = int(input())
+
+arr = array_random(n, min_array, max_array)
+print (arr)
+print("Введите минимальное значение диапазона :")
+min = int(input())
+print("Введите максимальное значение диапазона :")
+max = int(input())
+for i in range (len(arr)) :
+    if  arr[i] <= max and arr[i] >= min :
+     print (i , end = " ")
